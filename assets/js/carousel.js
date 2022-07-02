a4074774234c5dece443540af28c0a84fdcee697
hialdev <al.developer12@gmail.com>
2022-07-02 13:31:42 +0700

    new update

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -7,6 +7,25 @@ $('.big-news').owlCarousel({
     dots:true,
 });
 
+$('.featured-news').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:2,
+        }
+    }
+});
+
 var review = $('.review-card');
 
 review.owlCarousel({
